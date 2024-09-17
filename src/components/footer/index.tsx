1dedf8e6a66ebc6ca7086a272ab6b732a536716c
Felipe Sanchez <felipe.sdiseno@gmail.com>
2024-09-17 09:03:36 -0500

    push changes

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <div>
+        <h1>este es el footer</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,9 +1,26 @@
 import React from "react";
-
+import { FaRegUserCircle } from "react-icons/fa";
+import Link from "next/link";
 function NavBar() {
   return (
-    <div>
-      <h1>esta es la navbar</h1>
+    <div className="flex justify-between w-full m-x-auto p-2 mt-2">
+      <div className="flex flex-row items-center gap-5 ">
+        <div className="flex flex-row items-center">
+          <img src="/mjp1.png" alt="img mjp" className="w-[45px] h-[45px]" />
+          <div className="flex flex-col">
+            <h2>MOVIMIENTO </h2>
+            <h2>JUVENIL PEREGRINOS</h2>
+          </div>
+        </div>
+        <div className="flex flex-row gap-5">
+          <Link href="/">Inicio</Link>
+          <Link href="/aboutUs">Nustra comunidad</Link>
+          <Link href="/">Donaciones</Link>
+          <Link href="/">Eventos</Link>
+
+          <FaRegUserCircle />
+        </div>
+      </div>
     </div>
   );
 }
