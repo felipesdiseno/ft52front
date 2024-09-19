e6aa8555e2eaf83681b0039d945ca040b3d119a5
Felipe Sanchez <felipe.sdiseno@gmail.com>
2024-09-18 19:30:00 -0500

    eventspage add

---
+++ src/components/dropDownEvents/searchBar.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
+export function SearchBar() {
+  return (
+    <div className='flex w-auto max-w-sm items-center space-x-2'>
+      <Input type='text' placeholder='' />
+      <Button type='submit' className='bg-blue-500 hover:bg-blue-600'>
+        Buscar
+      </Button>
+    </div>
+  )
+}
