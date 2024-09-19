d1114c91ba54dc1b1d22aa7adafa06660bfaedf0
LorMatias <156853697+LorMatias@users.noreply.github.com>
2024-09-19 15:57:20 -0300

    Cards modularizadas

--- src/app/eventsPage/page.tsx
+++ src/app/eventsPage/page.tsx
@@ -1,8 +1,35 @@
-import { ComboboxDemo } from '@/components/dropDownEvents/monthFilter'
-import { SearchBar } from '@/components/dropDownEvents/searchBar'
-import { ComboboxDemoYear } from '@/components/dropDownEvents/yearFilter'
+import { ComboboxDemo } from '@/components/dropDownEvents/monthFilter';
+import { SearchBar } from '@/components/dropDownEvents/searchBar';
+import { ComboboxDemoYear } from '@/components/dropDownEvents/yearFilter';
+import EventsList from '@/components/events/eventsList';
 
 function EventsPage() {
+  const upcomingEvents = [
+    {
+      id: 1,
+      title: 'Primer reunión',
+      date: '2024-09-24',
+      time: '10:00 AM',
+      location: 'Centro',
+      imageUrl: '/placeholder.svg',
+    },
+    {
+      id: 2,
+      title: 'Segunda reunión',
+      date: '2024-10-02',
+      time: '7:00 PM',
+      location: 'Sala de conferencias',
+      imageUrl: '/placeholder.svg',
+    },
+    {
+      id: 3,
+      title: 'Tercera reunión',
+      date: '2024-10-10',
+      time: '2:00 PM',
+      location: 'Plaza',
+      imageUrl: '/placeholder.svg',
+    },
+  ];
   return (
     <div className='w-full'>
       <div className='container mx-auto flex flex-col '>
@@ -11,9 +38,10 @@ function EventsPage() {
           <ComboboxDemo />
           <ComboboxDemoYear />
           <SearchBar />
+          <EventsList events={upcomingEvents} />
         </div>
       </div>
     </div>
-  )
+  );
 }
-export default EventsPage
+export default EventsPage;
--- src/components/ui/CarouselSize.tsx
+++ src/components/ui/CarouselSize.tsx
@@ -1,13 +1,13 @@
-"use client";
-import * as React from "react";
-import { Card, CardContent } from "@/src/components/ui/card";
+'use client';
+import * as React from 'react';
+import { Card, CardContent } from '@/components/ui/card';
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/src/components/ui/carousel";
+} from '@/components/ui/carousel';
 
 // Imágenes para el carrusel
 const images = [
@@ -22,26 +22,26 @@ export function CarouselSize() {
   return (
     <Carousel
       opts={{
-        align: "start",
+        align: 'start',
         loop: true,
       }}
-      className="w-full max-w-2xl"  
+      className='w-full max-w-2xl'
     >
       <CarouselContent>
         {images.map((image, index) => (
-          <CarouselItem key={index} className="relative w-full">
-            <img 
-              src={image} 
-              alt={`Slide ${index + 1}`} 
-              className="w-full h-60 object-cover" 
+          <CarouselItem key={index} className='relative w-full'>
+            <img
+              src={image}
+              alt={`Slide ${index + 1}`}
+              className='w-full h-60 object-cover'
             />
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious className="absolute left-0 top-1/2 transform -translate-y-1/2">
+      <CarouselPrevious className='absolute left-0 top-1/2 transform -translate-y-1/2'>
         &lt;
       </CarouselPrevious>
-      <CarouselNext className="absolute right-0 top-1/2 transform -translate-y-1/2">
+      <CarouselNext className='absolute right-0 top-1/2 transform -translate-y-1/2'>
         &gt;
       </CarouselNext>
     </Carousel>
