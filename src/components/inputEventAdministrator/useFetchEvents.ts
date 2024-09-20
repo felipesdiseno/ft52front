a464ba98ff940962ffeda06060c63ebb872abe04
LorMatias <156853697+LorMatias@users.noreply.github.com>
2024-09-20 12:03:54 -0300

    fetch events

---
+++ src/components/inputEventAdministrator/useFetchEvents.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+
+interface Event {
+  id: number;
+  title: string;
+  date: string;
+  time: string;
+  location: string;
+  imageUrl: string;
+}
+
+export function useFetchEvents() {
+  const [events, setEvents] = useState<Event[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchEvents() {
+      try {
+        const response = await fetch('http://localhost:3003/events');
+        if (!response.ok) {
+          throw new Error('Error fetching events');
+        }
+        const data = await response.json();
+        setEvents(data);
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchEvents();
+  }, []);
+
+  return { events, loading, error };
+}
