2b2408bd673145e394c73b4c0a16bd2c1610827f
RodrigoFernandez <rodd_2610@hotmail.com>
2024-09-28 15:13:37 -0300

    PaginaQueRedirige al detalle del evento

--- src/app/eventdetail/[id]/page.tsx
+++ src/app/eventdetail/[id]/page.tsx
@@ -1,15 +1,34 @@
-import EventCard from "@/components/events/eventCard";
-import EventsList from "@/components/events/eventsList";
+// Asegúrate de que la ruta sea correcta
+import EventCardDetail from '@/components/eventDetail/indext';
+import getEventById from '@/utils/eventsdetail';
 
-function eventDetail(){
-
-
-    return(
-       <>
-      
-
-       </>
-    )
+interface Event {
+  id: string;
+  highlight: boolean;
+  createDate: Date;
+  status: string;
+  title: string;
+  eventDate: Date;
+  eventLocation: string;
+  description: string;
+  price: number;
+  stock: number;
+  images: string[];
 }
 
-export default eventDetail;
+const EventDetailPage = async ({ params }: { params: { id: string } }) => {
+  try {
+    const event: Event | null = await getEventById(params.id); // Cambiar a Event
+
+    if (!event) {
+      return <div>Evento no disponible</div>;
+    }
+    return <EventCardDetail {...event} />; // Pasar directamente las propiedades de event
+
+  } catch (error) {
+    console.error("Error", error);
+    return <div>Ha ocurrido un error al cargar el evento</div>;
+  }
+};
+
+export default EventDetailPage;
